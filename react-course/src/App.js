fc1576ccba9331d9a1ec53caf617f5185617dc63
Thomas Lamb <thomaslambprogramming@gmail.com>
2022-04-17 16:28:42 +1000

    Losing my mind with conditional operators and {} logic

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,43 +1,52 @@
 import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense"; 
+import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const logExpense = (newExpense) => {
-    console.log(newExpense);
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
   };
-
 
   return (
     //react allows only one root component eg div and etc otherwise it will not accept it. (cause that isnt really a single component then)
     <div>
-      <NewExpense onAddExpense={logExpense}/>
+      <NewExpense onAddExpense={logExpense} />
       {/* REACT WILL LOOK FOR CUSTOM COMPONENTS WHEN IT STARTS WITH A CAPITAL THATS WHY WE CAPITALISE IT
       LOWERCASE WILL BE READ AS HTML BECAUSE HTML IS ALL LOWERCASE eg. div, h1 etc */}
-        <Expenses expenses={expenses} expenseAmount={4} />
+      <Expenses expenses={expenses} expenseAmount={4} />
     </div>
   );
-}
+};
 
 export default App;
--- react-course/src/components/Expenses/Expenses.js
+++ react-course/src/components/Expenses/Expenses.js
@@ -8,9 +8,31 @@ import { useState } from "react";
 
 function Expenses(props) {
   const [currentYear, setCurrentYear] = useState("2022");
+
   const onYearChangeHandler = (newYear) => {
     setCurrentYear(newYear);
   };
+
+  const filteredContent = props.expenses.filter(expense => {return expense.date.getFullYear().toString() === currentYear})
+
+  //doing it this way is nicer, less horrid code in the return
+  let expensesContent = <p>There is no content for this year</p>;
+
+  if (filteredContent.length > 0){
+      expensesContent = filteredContent.map((expense) => (
+        <ExpenseItem
+        //Key is important to render dynamic lists without updating the whole list, try to not use index from map
+        //this is done so that react knows the order and the difference between list items so it basically does well this is key 1 changing
+        //but since key 2 is not being affected then we dont want to change that this key can be put on any component including html
+          key={expense.id}
+          //key needs any primitive
+
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />))
+        }
+      
 
   return (
     <Card className="expenses">
@@ -19,15 +41,8 @@ function Expenses(props) {
           selected={currentYear}
           onYearChange={onYearChangeHandler}
         />
+        {expensesContent}
       </div>
-
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
     </Card>
   );
 }
